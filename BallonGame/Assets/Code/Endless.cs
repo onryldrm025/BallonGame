0cc3f793b916dd8f69608de39e4a9bd8fe7dd20f
Hüseyin Emre KANTAŞ <huseyinemrekantas@gmail.com>
2019-04-23 17:58:27 +0300

    Revert "Revert "Initial Commit""
    This reverts commit 5d2094ddaaf3a00dd874223e6baf1bad7f1c67f7.

---
+++ BallonGame/Assets/Code/Endless.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endless : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log(collision.tag);
+
+
+    }
+
+
+
+
+}
