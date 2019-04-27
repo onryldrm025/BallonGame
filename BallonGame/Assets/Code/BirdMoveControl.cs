217928fea4914b1a2d3a9741ef921beb1ea2d7f5
Hüseyin Emre KANTAŞ <huseyinemrekantas@gmail.com>
2019-04-27 13:24:55 +0300

    27.04.2019.v1
    Bir çok şey.

---
+++ BallonGame/Assets/Code/BirdMoveControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMoveControl : MonoBehaviour
+{
+    Rigidbody2D physics;
+    public float speedHorizontal;//yatayda hareket değişkeni
+    public float speedVertical;//dikeyde hareket değişkeni
+    public float time;//Ne kadar süre boyunca gideceği
+    void Start()
+    {
+        physics = GetComponent<Rigidbody2D>();
+        InvokeRepeating("MoveControl", 0.1f, time);
+
+    }
+    void Update()
+    {
+
+        physics.velocity = new Vector2(speedHorizontal, speedVertical);
+    }
+    void MoveControl()
+    {
+
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);//Kuşun terse döndüğünde transformları ile oynayarak yönünü düzeltmek
+
+        speedHorizontal = -speedHorizontal;
+        speedVertical = -speedVertical;
+
+
+    }
+}
